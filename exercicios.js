86e5f69f7ac1dfecf2c38af686d9ab8b24507d0b
Amanda Yu <37625520+amandayuu@users.noreply.github.com>
2022-10-27 13:06:47 +0000

    exercicio 1 map

---
+++ exercicios.js
@@ -0,0 +1,11 @@
+//  EXERCÍCIO MAP
+
+// 1 - Criar uma nova lista com todos os itens da lista abaixo multiplicados por 2
+// Lista de Números - [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(value.map( item => item * 2))
+
+______________________________________________________________________________________
+
+
